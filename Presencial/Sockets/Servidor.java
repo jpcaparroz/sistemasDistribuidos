b43341a98ad17e4a8f3197817780a01f6439a6d1
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2022-09-15 23:16:45 -0300

    Sockets enviando e recebendo Objetos (Classes)

---
+++ Presencial/Sockets/Servidor.java
@@ -0,0 +1,59 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+
+    static ServerSocket serverSocket;
+    static Socket clienteSocket;
+
+    static Conexao conexao;
+    static String msg;
+
+    public Servidor() {
+
+        try {
+            serverSocket = new ServerSocket(9600);
+
+            System.out.println("Criando o ServerSocket");
+
+        } catch (Exception e) {
+
+            System.out.println("Erro ao criar ServerSocket");
+        }
+
+    }
+
+    static boolean connect() {
+        try {
+            clienteSocket = serverSocket.accept();
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        new Servidor();
+
+        Protocolo protocolo = new Protocolo();
+        
+        conexao = new Conexao();
+
+        
+
+        if (connect()) {
+
+            protocolo = (Protocolo) conexao.receive(clienteSocket);
+            protocolo.setResposta("OBRIGADO!");
+
+            System.out.println("Cliente respondeu no dia " + protocolo.getDia());
+
+            conexao.send(clienteSocket, protocolo);
+            
+        }
+    }
+
+}
